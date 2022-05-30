c4407d438a06ea65083c1dbc2979360ba0e1afae
code-with-onye <onyemachitrust@gmail.com>
2022-05-30 16:38:34 -0700

    size for large sc

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,23 +4,41 @@ export interface Props extends HtmlHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 
   /**  Which variant look do you like to use*/
-  variant: 'dark' | 'default' | "light";
-  size: 'small'| "medium" | "large" | "x-large";
+  variant: 'dark' | 'default' | 'light';
+  size: 'small' | 'medium' | 'large' | 'x-large';
 }
 
 /**  Button  a text*/
 
-const styleButton ={
+const styleButton = {
   dark: ` text-white bg-gray-800 hover:bg-gray-900   focus:ring-gray-300    dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`,
   default: `text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300     dark:bg-blue-600 dark:hover:bg-blue-700  dark:focus:ring-blue-800`,
   light: `   text-gray-900 bg-white  border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10  focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700`,
-}
+};
 
+export const Button = ({
+  children,
+  variant = 'default',
+  size,
+  ...props
+}: Props) => {
+  const buttonSize = `${
+    size === 'small'
+      ? 'px-4 py-2'
+      : size === 'medium'
+      ? 'px-6 py-3'
+      : size === 'large'
+      ? 'px-8 py-4'
+      : 'px-12 py-4'
+  }`;
+  const buttonColor = `${
+    variant === 'dark'
+      ? styleButton.dark
+      : variant === 'default'
+      ? styleButton.default
+      : styleButton.light
+  }`;
 
-export const Button = ({ children, variant = 'default', size, ...props }: Props) => {
-const buttonSize = `${size === "small" ? "px-4 py-2": "px-6 py-3"}`
-const buttonColor = `${variant === 'dark' ? styleButton.dark : variant=== "default" ? styleButton.default : styleButton.light}`
-  
   return (
     <button
       {...props}
